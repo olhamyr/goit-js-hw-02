8f04ece26b8cfff48520f9672695bfdf2da9d96c
Olha Myroshnychenko <olhamyr001@gmail.com>
2021-01-18 17:46:36 +0200

    task2

---
+++ js/task-2.js
@@ -0,0 +1,24 @@
+const calculateEngravingPrice = (message = '', pricePerWord = 0) =>
+  pricePerWord * message.split(" ").length; 
+
+console.log(
+  calculateEngravingPrice(
+    'Proin sociis natoque et magnis parturient montes mus',
+    10,),
+);
+
+console.log(
+    calculateEngravingPrice(
+      'Proin sociis natoque et magnis parturient montes mus',
+      20,),
+  );
+
+  console.log(
+    calculateEngravingPrice('Donec orci lectus aliquam est magnis',
+     40),
+  );
+
+  console.log(
+    calculateEngravingPrice('Donec orci lectus aliquam est magnis', 
+    20),
+  );
