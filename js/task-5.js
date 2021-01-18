697da194aea57c9bc22b03cf70cf0819d61b2419
Olha Myroshnychenko <olhamyr001@gmail.com>
2021-01-18 17:47:01 +0200

    task5

---
+++ js/task-5.js
@@ -0,0 +1,17 @@
+function checkForSpam(str) {
+    'use strict';
+    
+    const message = str.toLowerCase();
+    if (message.includes('spam') || message.includes('sale')) {
+        return true;
+    }
+    return false;
+}
+
+console.log(checkForSpam('Latest technology news')); // false
+
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+
+console.log(checkForSpam('Get best sale offers now!')); // true
+
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
