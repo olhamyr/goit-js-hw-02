5d1991b240dd2ac827daba997c3f426c16129b75
Olha Myroshnychenko <olhamyr001@gmail.com>
2021-01-18 17:46:53 +0200

    task4

---
+++ js/task-4.js
@@ -0,0 +1,15 @@
+  
+function formatString(string, maxLength = 40) {
+    if (string.length <= maxLength) {
+      return string;
+    }
+    return string.slice(0, maxLength) + '...';
+  }
+
+  console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
+
+  console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
+
+  console.log(formatString('Curabitur ligula sapien.'));
+
+  console.log(formatString('Nunc sed turpis. Curabitur a felis in nunc fringilla tristique.'));
